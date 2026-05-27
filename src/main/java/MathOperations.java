635126282176a2514da35cab93c8cbd80507d5a7
Kislovich23 <vlad-kislov-2016@mail.ru>
2026-05-27 22:40:14 +0500

    Кислов В.И. Задание 2.6

---
+++ src/main/java/MathOperations.java
@@ -0,0 +1,40 @@
+public class MathOperations {
+
+    // 1. Факториал числа
+    public static long factorial(int n) {
+        if (n < 0) throw new IllegalArgumentException("Число должно быть неотрицательным");
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 2. Площадь треугольника
+    public static double triangleArea(double base, double height) {
+        if (base <= 0 || height <= 0) throw new IllegalArgumentException("Размеры должны быть больше 0");
+        return 0.5 * base * height;
+    }
+
+    // 3. Арифметические действия
+    public static double calculate(int a, int b, char operation) {
+        switch (operation) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) throw new ArithmeticException("Деление на ноль");
+                return (double) a / b;
+            default:
+                throw new IllegalArgumentException("Неверная операция");
+        }
+    }
+
+    // 4. Сравнение двух чисел
+    public static int compare(int a, int b) {
+        return Integer.compare(a, b);
+    }
+}
